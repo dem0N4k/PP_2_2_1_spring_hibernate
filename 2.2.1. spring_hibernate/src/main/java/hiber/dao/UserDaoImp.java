e5af8e21142cee4b8d10b56c98fcf781fbaa90a6
dem0N4k <demON4k1993@gmail.com>
2023-11-24 13:50:26 +0300

    finish

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,5 +1,6 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,19 +12,42 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
+    @Override
+    public void add(Car car) {
+        sessionFactory.getCurrentSession().save(car);
+    }
+
+    @Override
+    public void getUserByCarModelAndSerial(String carModel, int carSeries) {
+        String HQL = "select id from Car c where c.model =:carModel and c.series =:carSeries";
+        Long id = (Long) sessionFactory.getCurrentSession().createQuery(HQL)
+                .setParameter("carModel", carModel)
+                .setParameter("carSeries", carSeries).uniqueResult();
+
+        String HQL2 = "from User u where u.id =:Id";
+        User user = (User) sessionFactory.getCurrentSession().createQuery(HQL2)
+                .setParameter("Id", id).uniqueResult();
+        System.out.println(user.toString());
+    }
+
+    @Override
+    public List<Car> listCars() {
+        TypedQuery<Car> query = sessionFactory.getCurrentSession().createQuery("from Car");
+        return query.getResultList();
+    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -1,10 +1,18 @@
 package hiber.service;
 
+import hiber.model.Car;
 import hiber.model.User;
 
 import java.util.List;
 
 public interface UserService {
     void add(User user);
+
     List<User> listUsers();
+
+    void add(Car car);
+
+    void getUserByCarModelAndSerial(String carModel, int carSeries);
+
+    List<Car> listCars();
 }
